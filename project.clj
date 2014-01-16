a052beafccaec11fe35930aba8ca4937afc2088e
Cameron Desautels <camdez@gmail.com>
2014-01-15 22:28:31 -0600

    Update to Clojure 1.4.0

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject tofu "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/tools.reader "0.7.3"]
                  [jline/jline "2.10"]]
   :main tofu.core)
