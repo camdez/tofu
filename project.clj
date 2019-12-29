177af438dff2569067610430276b21aab3871644
Cameron Desautels <camdez@gmail.com>
2019-12-29 17:05:43 -0600

    Upgrade dependencies

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject tofu "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [jline/jline "2.10"]]
-  :profiles {:dev {:dependencies [[midje "1.6.3"]]}}
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [jline/jline "3.0.0.M1"]]
+  :profiles {:dev {:dependencies [[midje "1.9.9"]]}}
   :main tofu.core)
