82c472464ba28d6eece3f0d526ccc18a8480b40b
Cameron Desautels <camdez@gmail.com>
2019-12-29 17:06:38 -0600

    project.clj: add description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject tofu "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "A simple command line to-do list manager."
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [jline/jline "3.0.0.M1"]]
   :profiles {:dev {:dependencies [[midje "1.9.9"]]}}
