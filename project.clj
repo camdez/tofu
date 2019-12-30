e2718b690d4878c9c722f96da23e9356bc32483e
Cameron Desautels <camdez@gmail.com>
2019-12-29 18:01:58 -0600

    Remove unused jline dependency

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
 (defproject tofu "1.0.0-SNAPSHOT"
   :description "A simple command line to-do list manager."
-  :dependencies [[org.clojure/clojure "1.10.1"]
-                 [jline/jline "3.0.0.M1"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]]
   :profiles {:dev {:dependencies [[midje "1.9.9"]]}}
   :main tofu.core)
